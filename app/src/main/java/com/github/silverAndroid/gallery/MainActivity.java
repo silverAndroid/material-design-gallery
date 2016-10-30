451858e60767c548f26d165feb612d8d52b0ee31
Rushil Perera <pererarushil1940@gmail.com>
2016-10-30 11:39:51 -0400

    Moved all splash screen logic to MainActivity

--- app/src/main/java/com/github/silverAndroid/gallery/MainActivity.java
+++ app/src/main/java/com/github/silverAndroid/gallery/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.github.silverAndroid.gallery.models.Album;
 import com.github.silverAndroid.gallery.models.Photo;
@@ -22,6 +25,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private PresenterLayer presenterLayer;
 
     @Override
@@ -34,10 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
         Hawk.init(getBaseContext()).build();
         boolean setupCompleted = Hawk.get("setupCompleted", false);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
         final boolean[] loadedAlbums = {false}, loadedPhotos = {false}, failedAlbums = {false}, failedPhotos = {false};
         if (setupCompleted) {
-
+            loadFragment(false, false);
         } else {
+            progressBar.setVisibility(View.VISIBLE);
             presenterLayer.loadAlbumsFromNetwork(new Callback<List<Album>>() {
                 @Override
                 public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
@@ -52,16 +58,18 @@ public class MainActivity extends AppCompatActivity {
                     loadedAlbums[0] = true;
 
                     if (loadedPhotos[0]) {
-                        goToNextActivity(failedAlbums[0], failedPhotos[0]);
+                        loadFragment(failedAlbums[0], failedPhotos[0]);
                     }
                 }
 
                 @Override
                 public void onFailure(Call<List<Album>> call, Throwable t) {
                     failedAlbums[0] = true;
+                    loadedAlbums[0] = true;
+                    Log.e(TAG, "onFailure: Failed to load albums", t);
 
                     if (loadedPhotos[0]) {
-                        goToNextActivity(failedAlbums[0], failedPhotos[0]);
+                        loadFragment(failedAlbums[0], failedPhotos[0]);
                     }
                 }
             });
@@ -80,25 +88,25 @@ public class MainActivity extends AppCompatActivity {
                     loadedPhotos[0] = true;
 
                     if (loadedAlbums[0]) {
-                        goToNextActivity(failedAlbums[0], failedPhotos[0]);
+                        loadFragment(failedAlbums[0], failedPhotos[0]);
                     }
                 }
 
                 @Override
                 public void onFailure(Call<List<Photo>> call, Throwable t) {
                     failedPhotos[0] = true;
+                    loadedPhotos[0] = true;
+                    Log.e(TAG, "onFailure: Failed to load albums", t);
 
                     if (loadedAlbums[0]) {
-                        goToNextActivity(failedAlbums[0], failedPhotos[0]);
+                        loadFragment(failedAlbums[0], failedPhotos[0]);
                     }
                 }
             });
         }
+    }
 
-        Intent intent = getIntent();
-        boolean failedAlbums = intent.getBooleanExtra("albumsFailed", false);
-        boolean failedPhotos = intent.getBooleanExtra("photosFailed", false);
-
+    private void loadFragment(boolean failedAlbums, boolean failedPhotos) {
         if (failedAlbums || failedPhotos) {
             new AlertDialog.Builder(this).setTitle("Error").setMessage("Unable to retrieve images. Try again later.")
                     .show();
