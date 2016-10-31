fd9abe0a10aa6e066c5881815130904aeaba676c
Rushil Perera <pererarushil1940@gmail.com>
2016-10-31 06:37:53 -0400

    Moved getColumn to utility class

---
+++ app/src/main/java/com/github/silverAndroid/gallery/Util.java
@@ -0,0 +1,22 @@
+package com.github.silverAndroid.gallery;
+
+/**
+ * Created by silver_android on 30/10/16.
+ */
+
+public class Util {
+
+    public static String getColumn(String table, String column) {
+        return getColumn(table, column, null);
+    }
+
+    public static String getColumn(String table, String column, String alias) {
+        if (table == null || table.isEmpty())
+            throw new RuntimeException("Table cannot be empty!");
+        String query = String.format("%s.%s", table, column);
+        if (query != null && !query.isEmpty()) {
+            query += String.format(" AS %s", alias);
+        }
+        return query;   
+    }
+}
