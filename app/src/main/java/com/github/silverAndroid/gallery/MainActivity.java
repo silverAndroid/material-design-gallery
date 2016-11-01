b3e2710edffe9ffdffb7fda4e22975b665140f72
Rushil Perera <pererarushil1940@gmail.com>
2016-10-31 20:56:42 -0400

    Moved Fresco initialization to MyApplication, added Fragment listeners, moved network loading to separate method

--- app/src/main/java/com/github/silverAndroid/gallery/MainActivity.java
+++ app/src/main/java/com/github/silverAndroid/gallery/MainActivity.java
@@ -1,16 +1,18 @@
 package com.github.silverAndroid.gallery;
 
 import android.content.ContentValues;
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.util.DiffUtil;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.github.silverAndroid.gallery.models.Album;
 import com.github.silverAndroid.gallery.models.Photo;
@@ -23,10 +25,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AlbumsFragment.AlbumSelectedListener, AlbumFragment.PhotoSelectedListener,
+        FragmentManager.OnBackStackChangedListener {
 
     private static final String TAG = "MainActivity";
     private PresenterLayer presenterLayer;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,85 +42,38 @@ public class MainActivity extends AppCompatActivity {
 
         Hawk.init(getBaseContext()).build();
         boolean setupCompleted = Hawk.get("setupCompleted", false);
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
-        final boolean[] loadedAlbums = {false}, loadedPhotos = {false}, failedAlbums = {false}, failedPhotos = {false};
+        progressBar = (ProgressBar) findViewById(R.id.progress);
         if (setupCompleted) {
             loadFragment(false, false);
         } else {
             progressBar.setVisibility(View.VISIBLE);
-            presenterLayer.loadAlbumsFromNetwork(new Callback<List<Album>>() {
-                @Override
-                public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
-                    ContentValues[] values = new ContentValues[response.body().size()];
-                    List<Album> body = response.body();
-                    for (int i = 0, bodySize = body.size(); i < bodySize; i++) {
-                        Album album = body.get(i);
-                        values[i] = album.convert();
-                    }
-                    getContentResolver().bulkInsert(GalleryDBProvider.Album.CONTENT_URI, values);
-
-                    loadedAlbums[0] = true;
-
-                    if (loadedPhotos[0]) {
-                        loadFragment(failedAlbums[0], failedPhotos[0]);
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<List<Album>> call, Throwable t) {
-                    failedAlbums[0] = true;
-                    loadedAlbums[0] = true;
-                    Log.e(TAG, "onFailure: Failed to load albums", t);
-
-                    if (loadedPhotos[0]) {
-                        loadFragment(failedAlbums[0], failedPhotos[0]);
-                    }
-                }
-            });
-
-            presenterLayer.loadPhotosFromNetwork(new Callback<List<Photo>>() {
-                @Override
-                public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
-                    ContentValues[] values = new ContentValues[response.body().size()];
-                    List<Photo> body = response.body();
-                    for (int i = 0, bodySize = body.size(); i < bodySize; i++) {
-                        Photo photo = body.get(i);
-                        values[i] = photo.convert();
-                    }
-                    getContentResolver().bulkInsert(GalleryDBProvider.Photo.CONTENT_URI, values);
-
-                    loadedPhotos[0] = true;
-
-                    if (loadedAlbums[0]) {
-                        loadFragment(failedAlbums[0], failedPhotos[0]);
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<List<Photo>> call, Throwable t) {
-                    failedPhotos[0] = true;
-                    loadedPhotos[0] = true;
-                    Log.e(TAG, "onFailure: Failed to load albums", t);
-
-                    if (loadedAlbums[0]) {
-                        loadFragment(failedAlbums[0], failedPhotos[0]);
-                    }
-                }
-            });
+            loadFromNetwork();
         }
+
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
     }
 
     private void loadFragment(boolean failedAlbums, boolean failedPhotos) {
+        Log.i(TAG, "loadFragment: failedAlbums = " + failedAlbums + ", failedPhotos = " + failedPhotos);
         if (failedAlbums || failedPhotos) {
-            new AlertDialog.Builder(this).setTitle("Error").setMessage("Unable to retrieve images. Try again later.")
-                    .show();
+            Toast.makeText(getBaseContext(), "Unable to retrieve images. Try again later.", Toast.LENGTH_LONG).show();
         } else {
-            showAlbumFragment();
+            Hawk.put("setupCompleted", true);
+            showAlbumsFragment();
         }
+        progressBar.setVisibility(View.GONE);
     }
 
-    private void showAlbumFragment() {
-        showFragment(AlbumFragment.newInstance(presenterLayer), false);
+    private void showAlbumsFragment() {
+        showFragment(AlbumsFragment.newInstance(this), false);
+    }
+
+    private void showAlbumFragment(int albumID) {
+        showFragment(AlbumFragment.newInstance(albumID, this), true);
+    }
+
+    private void showPhotoFragment(int photoID) {
+        showFragment(PhotoFragment.newInstance(photoID), true);
     }
 
     private void showFragment(Fragment fragment, boolean addToStack) {
@@ -127,5 +84,94 @@ public class MainActivity extends AppCompatActivity {
         if (addToStack)
             transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getSupportFragmentManager().popBackStack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void albumSelected(int albumID) {
+        showAlbumFragment(albumID);
+    }
+
+    @Override
+    public void photoSelected(int photoID) {
+        showPhotoFragment(photoID);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        int backStackCount = getSupportFragmentManager().getBackStackEntryCount();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(backStackCount != 0);
+    }
+
+    private void loadFromNetwork() {
+
+        final boolean[] loadedAlbums = {false}, loadedPhotos = {false}, failedAlbums = {false}, failedPhotos = {false};
+        presenterLayer.loadAlbums(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> body = response.body();
+                int bodySize = body.size();
+                ContentValues[] values = new ContentValues[bodySize];
+                for (int i = 0; i < bodySize; i++) {
+                    Album album = body.get(i);
+                    values[i] = album.convert();
+                }
+                getContentResolver().bulkInsert(GalleryDBProvider.Album.CONTENT_URI, values);
+
+                loadedAlbums[0] = true;
+                if (loadedPhotos[0]) {
+                    loadFragment(failedAlbums[0], failedPhotos[0]);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+                failedAlbums[0] = !call.isCanceled();
+                loadedAlbums[0] = true;
+                Log.e(TAG, "onFailure: Failed to load albums", t);
+
+                if (loadedPhotos[0]) {
+                    loadFragment(failedAlbums[0], failedPhotos[0]);
+                }
+            }
+        });
+
+        presenterLayer.loadPhotos(new Callback<List<Photo>>() {
+            @Override
+            public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
+                List<Photo> body = response.body();
+                int bodySize = body.size();
+                ContentValues[] values = new ContentValues[bodySize];
+                for (int i = 0; i < bodySize; i++) {
+                    Photo photo = body.get(i);
+                    values[i] = photo.convert();
+                }
+                getContentResolver().bulkInsert(GalleryDBProvider.Photo.CONTENT_URI, values);
+
+                loadedPhotos[0] = true;
+                if (loadedAlbums[0]) {
+                    loadFragment(failedAlbums[0], failedPhotos[0]);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Photo>> call, Throwable t) {
+                failedPhotos[0] = !call.isCanceled();
+                loadedPhotos[0] = true;
+                Log.e(TAG, "onFailure: Failed to load albums", t);
+
+                if (loadedAlbums[0]) {
+                    loadFragment(failedAlbums[0], failedPhotos[0]);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/github/silverAndroid/gallery/MyApplication.java
+++ app/src/main/java/com/github/silverAndroid/gallery/MyApplication.java
@@ -1,6 +1,18 @@
 package com.github.silverAndroid.gallery;
 
 import android.app.Application;
+import android.content.AsyncQueryHandler;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.facebook.common.logging.FLog;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.listener.RequestListener;
+import com.facebook.imagepipeline.listener.RequestLoggingListener;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by silver_android on 26/10/16.
@@ -14,6 +26,15 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         networkService = new NetworkService();
+
+        Set<RequestListener> requestListeners = new HashSet<>();
+        requestListeners.add(new RequestLoggingListener());
+        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+                .setDownsampleEnabled(true)
+                .setRequestListeners(requestListeners)
+                .build();
+        Fresco.initialize(this, config);
+        FLog.setMinimumLoggingLevel(FLog.VERBOSE);
     }
 
     public NetworkService getNetworkService() {
